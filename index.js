e65d816175e22710aa18e9a514a84581b4d8d336
Felix Becker <felix.b@outlook.com>
2015-09-19 20:46:13 +0200

    put values directly into object

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 module.exports = function SQL(strings) {
-  let values = Array.prototype.slice.call(arguments, 1) // since node doesnt support argument unpacking yet
   return {
     sql: strings.join('?'), // for mysql / mysql2
     text: strings.reduce((previous, current, i) => previous + '$' + i + current), // for postgres
-    values
+    values: Array.prototype.slice.call(arguments, 1) // since node doesnt support argument unpacking yet
   }
 }
