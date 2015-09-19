a383d4412a712829da7ddc673884b653816a27b7
Felix Becker <felix.b@outlook.com>
2015-09-19 22:37:12 +0200

    use Array.from()

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ module.exports = function SQL(strings) {
   return {
     sql: strings.join('?'), // for mysql / mysql2
     text: strings.reduce((previous, current, i) => previous + '$' + i + current), // for postgres
-    values: Array.prototype.slice.call(arguments, 1) // since node doesnt support argument unpacking yet
+    values: Array.from(arguments).slice(1) // since node doesnt support argument unpacking yet
   }
 }
