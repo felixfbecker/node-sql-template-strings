44f9b08d2134b76e1e8439a2294829232a067bfa
Felix Becker <felix.b@outlook.com>
2016-12-11 13:07:51 +0100

    Add integration tests (#17)

---
+++ test/integration/pg.js
@@ -0,0 +1,37 @@
+
+const SQL = require('../..')
+const pg = require('pg')
+const assert = require('assert')
+
+describe('pg', () => {
+  it('should work with a simple query', done => {
+    const client = new pg.Client(process.env.PG_CONN)
+    client.connect(err => {
+      if (err) {
+        return done(err)
+      }
+      client.query(SQL`SELECT ${1} + 1 as result`, (err, result) => {
+        if (err) {
+          return done(err)
+        }
+        assert.equal(result.rows[0].result, 2)
+        done()
+      })
+    })
+  })
+  it('should work with a named statement', done => {
+    const client = new pg.Client(process.env.PG_CONN)
+    client.connect(err => {
+      if (err) {
+        return done(err)
+      }
+      client.query(SQL`SELECT ${1} + 1 as result`.setName('my_query'), (err, result) => {
+        if (err) {
+          return done(err)
+        }
+        assert.equal(result.rows[0].result, 2)
+        done()
+      })
+    })
+  })
+})
---
+++ test/integration/sequelize.js
@@ -0,0 +1,19 @@
+
+const SQL = require('../..')
+const Sequelize = require('sequelize')
+const assert = require('assert')
+
+describe('sequelize', () => {
+  it('should work with a simple query', () => {
+    const sequelize = new Sequelize(process.env.PG_CONN, {logging: false})
+    return sequelize.query(SQL`SELECT ${1} + 1 as result`, {type: Sequelize.QueryTypes.SELECT}).then(rows => {
+      assert.equal(rows[0].result, 2)
+    })
+  })
+  it('should work with a bound statement', () => {
+    const sequelize = new Sequelize(process.env.PG_CONN, {logging: false})
+    return sequelize.query(SQL`SELECT ${1} + 1 as result`.useBind(true), {type: Sequelize.QueryTypes.SELECT}).then(rows => {
+      assert.equal(rows[0].result, 2)
+    })
+  })
+})
